da60137316e1be6e408adc1829bcf5ff3e997cb9
carlitos <cbeltran@libero.it>
2020-06-17 19:30:15 +0200

    Not quite right but somehow working
    The python reads the absolute rotation angles from the serial port in which the arduino (with a cabled BNO055) is connected  and rotates a cube. When moving the BNO055 the cube rotates.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # IMU_3DViewer
 Python client to visualize absolute orientation from a BNO055 by means of an Arduino Nano with Adafruit libraries
+
+## Details
+
+It read from the the serial port connected to an Arduino Nano. The Arduino
+Nano has been burned with the Examples/Adafruit BNO055/sensorapi sketch.
+The sketch sends over the serial port the absolute orientation in the form of:
+<code>
+X: 328.8750 Y: 3.1875 Z: -1.0000  
+</code> 
+Values are in degrees
+
+## Environment
+Windows 10
+Anaconda
+
+## Dependencies
+Python 3
+pyserial (pip install pyserial)
+vpython (pip install vpython) [vpython.org]
--- imuviewer.py
+++ imuviewer.py
@@ -1,7 +1,39 @@
 import serial
+import re
+from vpython import *
+import math
 
+encoding = 'utf-8'
 arduino = serial.Serial('COM13', 115200, timeout=.1)
+myboxpos = vector(0,0,0)
+mybox = box(pos=myboxpos,size = vector(1,1,1),color=color.red)
+xaxis = arrow(pos = vector(2,2,2), axis=vector(1,0,0),shaftwidth=0.1,color = color.red)
+yaxis = arrow(pos = vector(2,2,2), axis=vector(0,1,0),shaftwidth=0.1,color = color.green)
+zaxis = arrow(pos = vector(2,2,2), axis=vector(0,0,1),shaftwidth=0.1,color = color.blue)
+
+prev_x = 0.0
+prev_y = 0.0
+prev_z = 0.0
 while True:
-	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
-	if data:
-		print(data)
+    data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
+    if data:
+        datastr = data.decode(encoding)
+        #print(datastr.find("X"))
+        #print(datastr.find("Y"))
+        #print(datastr.find("Z"))
+        values = re.findall("\d+\.\d+",datastr)
+        if len(values) is 3:
+            print(values)
+            x = math.radians(float(values[0]))
+            y = math.radians(float(values[1]))
+            z = math.radians(float(values[2]))
+            dx = x - prev_x # Delta x
+            dy = y - prev_y # Delta y
+            dz = z - prev_z # Delta z
+            mybox.rotate(angle=dx, axis=vector(1,0,0))
+            mybox.rotate(angle=dy, axis=vector(0,1,0))
+            mybox.rotate(angle=dz, axis=vector(0,0,1))
+            prev_x = x
+            prev_y = y
+            prev_z = z
+
